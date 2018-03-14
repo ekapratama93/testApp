ff854911170bcb5a632d4ab3e45b1d5094c49251
ekapratama93 <ekapratama93@gmail.com>
2018-03-14 11:20:12 +0700

    init

---
+++ src/main/java/com/example/test/Application.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Application {
+    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String test(){
+        return "test";
+    }
+
+}
